efd68eaed99761e4920661ebcbb549dbf479cac9
Alvin Sartor <alvinsartor@gmail.com>
2022-01-07 19:13:09 +0100

    Copy messages over from backend

---
+++ frontend/messages/README.md
@@ -0,0 +1,29 @@
+# Messages
+
+This folder contains the messages signed by the users.
+
+Each message follows the EIP712 specifications.
+
+
+## Current Conditions
+
+### Balance
+
+Triggers the action whenever the user owns a certain amount of a token.
+
+### Frequency
+
+Triggers the action every fixed amount of blocks.
+
+
+## Current Actions
+
+### Swap
+
+Swaps a token for another one.
+Currently all swaps go through the same AMM, but in the future the user will have the possibility of specifying their favorite exchange.
+
+
+## Note:
+
+⚠️⚠️ This folder should always be in sync between frontend and backend! ⚠️⚠️
---
+++ frontend/messages/swap-action-messages.ts
@@ -0,0 +1,35 @@
+import { BigNumber } from 'ethers';
+import { IBalanceCondition, Balance } from './condition-messages';
+import { IFrequencyCondition, Frequency } from './condition-messages';
+
+export interface ISwapAction {
+    id: string;
+    tokenFrom: string;
+    tokenTo: string;
+    amount: BigNumber;
+    user: string;
+    executor: string;
+    balance: IBalanceCondition;
+    frequency: IFrequencyCondition;
+}
+
+const Swap = [
+    { name: "id", type: "bytes32" },                           // the token owned by the user
+    { name: "tokenFrom", type: "address" },             // the token owned by the user
+    { name: "tokenTo", type: "address" },                  // the token that should be swapped
+    { name: "amount", type: "uint256" },                   // the amount to swap
+    { name: "user", type: "address" },                        // the user that is signing the transaction
+    { name: "executor", type: "address" },                 // the executor contract this message will be sent to
+    { name: "balance", type: "Balance" },                   // condition: balance
+    { name: "frequency", type: "Frequency" },           // condition: frequency
+];
+
+export const domain = {
+    name: "Balrog-Swap-v1"
+};
+
+export const types = {
+    Frequency,
+    Balance,
+    Swap,
+};
