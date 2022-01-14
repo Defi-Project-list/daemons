4392aa078c045cc5e351e405673685f110a32f50
Alvin Sartor <alvinsartor@gmail.com>
2022-01-14 16:13:20 +0100

    Update contract addresses with recently deployed contracts

--- frontend/src/data/contracts.ts
+++ frontend/src/data/contracts.ts
@@ -1,10 +1,12 @@
 import { ZeroAddress } from './chain-info';
 
 const ProductionContracts = {
-    GasTank: '0xF16879A9E55e6c03E4Ff4d86F41DD0Dc2D299A06',
-    BRGToken: '0x701301aE25c130144c975Ce84Dd0B6C363f9C44f',
+    GasTank: '0x0D2a5C35ad867E6FBeaDD58977FD0ee42Fbb78aD',
+    BRGToken: '0x29Ff4c4297694f8b67df46Fc42216c17dE57f9eB',
+    PriceRetriever: '0x2FbBBd586eDC580F0dB8F9620db6E153b1aD1136',
 
-    SwapExecutor: ZeroAddress,
+    SwapExecutor: '0x2827Ba91D27136Aba3e6ad30aA7D1e6D1ED4Dd66',
+    TransferExecutor: '0x2d407fBCA4984621099664789EadCE26524B6335',
 };
 
 export const Contracts = ProductionContracts;
