24132eab21f566f51c4cef24b00e1f5306515d7f
Alvin Sartor <alvinsartor@gmail.com>
2022-01-14 16:12:58 +0100

    Split deploy scripts

---
+++ backend/scripts/deploy-swapper-executor.ts
@@ -0,0 +1,42 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+import { BigNumber } from 'ethers';
+import { ethers } from "hardhat";
+
+const testRouterAddress = '0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506';
+const gasTankAddress = '0x0D2a5C35ad867E6FBeaDD58977FD0ee42Fbb78aD';
+const balrogTokenAddress = '0x29Ff4c4297694f8b67df46Fc42216c17dE57f9eB';
+const priceRetrieverAddress = '0x2FbBBd586eDC580F0dB8F9620db6E153b1aD1136';
+
+async function main() {
+  const [owner] = await ethers.getSigners();
+  const initialBalance = await owner.getBalance();
+  console.log("Deploying contracts with the account:", owner.address);
+  console.log("Account balance:", initialBalance.div(BigNumber.from("10").pow(BigNumber.from("12"))).toNumber() / 1000000);
+
+  // Swapper Script Executor
+  const swapperScriptExecutorContract = await ethers.getContractFactory("SwapperScriptExecutor");
+  const swapperScriptExecutor = await swapperScriptExecutorContract.deploy();
+  await swapperScriptExecutor.deployed();
+  console.log(`SwapperScriptExecutor deployed to: ${swapperScriptExecutor.address}`);
+
+  await swapperScriptExecutor.setGasTank(gasTankAddress);
+  await swapperScriptExecutor.setBrgToken(balrogTokenAddress);
+  await swapperScriptExecutor.setPriceRetriever(priceRetrieverAddress);
+  await swapperScriptExecutor.setExchange(testRouterAddress);
+  console.log(`Secondary contracts have been set`);
+
+  const finalBalance = await owner.getBalance();
+  console.log("Deployment completed");
+  console.log("ETH spent in gas fees: ", initialBalance.sub(finalBalance).div(BigNumber.from("10").pow(BigNumber.from("12"))).toNumber() / 1000000);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
---
+++ backend/scripts/deploy-transfer-executor.ts
@@ -0,0 +1,40 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+import { BigNumber } from 'ethers';
+import { ethers } from "hardhat";
+
+const gasTankAddress = '0x0D2a5C35ad867E6FBeaDD58977FD0ee42Fbb78aD';
+const balrogTokenAddress = '0x29Ff4c4297694f8b67df46Fc42216c17dE57f9eB';
+const priceRetrieverAddress = '0x2FbBBd586eDC580F0dB8F9620db6E153b1aD1136';
+
+async function main() {
+  const [owner] = await ethers.getSigners();
+  const initialBalance = await owner.getBalance();
+  console.log("Deploying contracts with the account:", owner.address);
+  console.log("Account balance:", initialBalance.div(BigNumber.from("10").pow(BigNumber.from("12"))).toNumber() / 1000000);
+
+  // Transfer Script Executor
+  const transferScriptExecutorContract = await ethers.getContractFactory("TransferScriptExecutor");
+  const transferScriptExecutor = await transferScriptExecutorContract.deploy();
+  await transferScriptExecutor.deployed();
+  console.log(`TransferScriptExecutor deployed to: ${transferScriptExecutor.address}`);
+
+  await transferScriptExecutor.setGasTank(gasTankAddress);
+  await transferScriptExecutor.setBrgToken(balrogTokenAddress);
+  await transferScriptExecutor.setPriceRetriever(priceRetrieverAddress);
+  console.log(`Secondary contracts have been set`);
+
+  const finalBalance = await owner.getBalance();
+  console.log("Deployment completed");
+  console.log("ETH spent in gas fees: ", initialBalance.sub(finalBalance).div(BigNumber.from("10").pow(BigNumber.from("12"))).toNumber() / 1000000);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
