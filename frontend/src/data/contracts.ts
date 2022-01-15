a419c2c186834cee8d32ed0d9f32f4896a28b19e
Alvin Sartor <alvinsartor@gmail.com>
2022-01-15 21:01:58 +0100

    Redeploy contracts into Kovan and update references

--- frontend/src/data/contracts.ts
+++ frontend/src/data/contracts.ts
@@ -1,12 +1,10 @@
-import { ZeroAddress } from './chain-info';
-
 const ProductionContracts = {
     GasTank: '0x0D2a5C35ad867E6FBeaDD58977FD0ee42Fbb78aD',
     BRGToken: '0x29Ff4c4297694f8b67df46Fc42216c17dE57f9eB',
     PriceRetriever: '0x2FbBBd586eDC580F0dB8F9620db6E153b1aD1136',
 
-    SwapExecutor: '0x2827Ba91D27136Aba3e6ad30aA7D1e6D1ED4Dd66',
-    TransferExecutor: '0x2d407fBCA4984621099664789EadCE26524B6335',
+    SwapExecutor: '0x43075b39E09799Ec35a9bb7Bb7F2B97800325BFB',
+    TransferExecutor: '0xCEd7Db876508fBCd96F28D5f94ee2Ef6d73b8EeD',
 };
 
 export const Contracts = ProductionContracts;
