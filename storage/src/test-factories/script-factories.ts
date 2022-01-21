ab5bf6fb463ecd8ce89d1b3c5ca2e6377ef9649b
Alvin Sartor <alvinsartor@gmail.com>
2022-01-21 22:00:16 +0100

    [BRG-30] Increase factories randomization with Faker

--- storage/src/test-factories/script-factories.ts
+++ storage/src/test-factories/script-factories.ts
@@ -1,18 +1,20 @@
 import { BigNumber, utils } from 'ethers';
-import { IBalanceCondition, IFrequencyCondition, IPriceCondition } from '../../../messages/definitions/condition-messages';
+import { ComparisonType, IBalanceCondition, IFrequencyCondition, IPriceCondition } from '../../../messages/definitions/condition-messages';
 import { ISignedSwapAction } from '../../../messages/definitions/swap-action-messages';
 import { ISignedTransferAction } from '../../../messages/definitions/transfer-action-messages';
 import { SwapScript } from '../models/swap-script';
 import { TransferScript } from '../models/transfer-script';
+import faker from '@faker-js/faker';
 
-
+const randomEthAmount = () => utils.parseEther(faker.datatype.number({ min: 0.01, max: 10, precision: 0.01 }).toString());
+const randomBigNumber = () => BigNumber.from(faker.datatype.number({ min: 0, max: 10000 }).toString());
 
 /** Returns a randomized balance condition */
 function balanceConditionFactory(args: any): IBalanceCondition {
     return {
         enabled: args.enabled ?? false,
-        comparison: args.comparison ?? 0x01,
-        amount: args.amount ?? utils.parseEther("1"),
+        comparison: args.comparison ?? ComparisonType.GreaterThan,
+        amount: args.amount ?? randomEthAmount(),
         token: args.token ?? '0x596e8221a30bfe6e7eff67fee664a01c73ba3c56',
     };
 }
@@ -21,9 +23,9 @@ function balanceConditionFactory(args: any): IBalanceCondition {
 function priceConditionFactory(args: any): IPriceCondition {
     return {
         enabled: args.enabled ?? false,
-        comparison: args.comparison ?? 0x01,
-        value: args.value ?? utils.parseEther("1"),
-        token: args.token ?? '0x596e8221a30bfe6e7eff67fee664a01c73ba3c56',
+        comparison: args.comparison ?? ComparisonType.GreaterThan,
+        value: args.value ?? randomEthAmount(),
+        token: args.token ?? faker.finance.ethereumAddress(),
     };
 }
 
@@ -31,21 +33,21 @@ function priceConditionFactory(args: any): IPriceCondition {
 function frequencyConditionFactory(args: any): IFrequencyCondition {
     return {
         enabled: args.enabled ?? false,
-        blocks: args.blocks ?? BigNumber.from("1"),
-        startBlock: args.startBlock ?? BigNumber.from("0"),
+        blocks: args.blocks ?? randomBigNumber(),
+        startBlock: args.startBlock ?? randomBigNumber(),
     };
 }
 
 /** Returns a randomized signed swap action */
 export function signedSwapActionFactory(args: any): ISignedSwapAction {
     return {
-        signature: args.signature ?? 'IAmASignature',
+        signature: args.signature ?? utils.hexlify(utils.randomBytes(40)),
         scriptId: args.scriptId ?? utils.hexlify(utils.randomBytes(32)),
-        tokenFrom: args.tokenFrom ?? '0x000002',
-        tokenTo: args.tokenTo ?? '0x000003',
-        amount: args.amount ?? utils.parseEther("1"),
-        user: args.user ?? '0x9a2f243c605e6908d96b18e21fb82bf288b19ef3',
-        executor: args.executor ?? '0xd7754711773489f31a0602635f3f167826ce53c5',
+        tokenFrom: args.tokenFrom ?? faker.finance.ethereumAddress(),
+        tokenTo: args.tokenTo ?? faker.finance.ethereumAddress(),
+        amount: args.amount ?? randomEthAmount(),
+        user: args.user ?? faker.finance.ethereumAddress(),
+        executor: args.executor ?? faker.finance.ethereumAddress(),
         chainId: args.chainId ?? BigNumber.from("42"),
         balance: balanceConditionFactory(args.balance ?? {}),
         frequency: frequencyConditionFactory(args.frequency ?? {}),
@@ -56,13 +58,13 @@ export function signedSwapActionFactory(args: any): ISignedSwapAction {
 /** Returns a randomized signed transfer action */
 export function signedTransferActionFactory(args: any): ISignedTransferAction {
     return {
-        signature: args.signature ?? 'IAmASignature',
+        signature: args.signature ?? utils.hexlify(utils.randomBytes(40)),
         scriptId: args.scriptId ?? utils.hexlify(utils.randomBytes(32)),
-        token: args.tokenFrom ?? '0x000002',
-        destination: args.tokenTo ?? '0x000003',
-        amount: args.amount ?? utils.parseEther("1"),
-        user: args.user ?? '0x9a2f243c605e6908d96b18e21fb82bf288b19ef3',
-        executor: args.executor ?? '0xd7754711773489f31a0602635f3f167826ce53c5',
+        token: args.tokenFrom ?? faker.finance.ethereumAddress(),
+        destination: args.tokenTo ?? faker.finance.ethereumAddress(),
+        amount: args.amount ?? randomEthAmount(),
+        user: args.user ?? faker.finance.ethereumAddress(),
+        executor: args.executor ?? faker.finance.ethereumAddress(),
         chainId: args.chainId ?? BigNumber.from("42"),
         balance: balanceConditionFactory(args.balance ?? {}),
         frequency: frequencyConditionFactory(args.frequency ?? {}),
