a3672c08c67f59588ad408f710c9281d5f39ed35
Alvin Sartor <alvinsartor@gmail.com>
2022-01-21 19:09:59 +0100

    Change mongo db name

--- storage/src/index.ts
+++ storage/src/index.ts
@@ -3,7 +3,7 @@ import { app } from './app';
 
 
 mongoose
-    .connect("mongodb://localhost:27017/scripts")
+    .connect("mongodb://localhost:27017/balrog")
     .then(() => {
         app.listen(5000, () => {
             console.log("Storage has started!");
