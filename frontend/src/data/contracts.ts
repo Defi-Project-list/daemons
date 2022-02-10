cfbcb7126de693d148abb1bc543e9a63b45573de
Alvin Sartor <alvinsartor@gmail.com>
2022-02-10 13:36:24 +0100

    **update contracts**

--- frontend/src/data/contracts.ts
+++ frontend/src/data/contracts.ts
@@ -3,8 +3,8 @@ const ProductionContracts = {
     BRGToken: '0x29Ff4c4297694f8b67df46Fc42216c17dE57f9eB',
     PriceRetriever: '0x2FbBBd586eDC580F0dB8F9620db6E153b1aD1136',
 
-    SwapExecutor: '0x43075b39E09799Ec35a9bb7Bb7F2B97800325BFB',
-    TransferExecutor: '0xCEd7Db876508fBCd96F28D5f94ee2Ef6d73b8EeD',
+    SwapExecutor: '0xa11AF4FB81E0944302c484166d221E0d4661e6D7',
+    TransferExecutor: '0xd6cB9F771861D7790292D80F1f2ba4Ca4d255c91',
 };
 
 export const Contracts = ProductionContracts;
