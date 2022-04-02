549bcb936ccb82c25185f04c2d8e32c0abbfe803
Alvin Sartor <alvinsartor@gmail.com>
2022-04-02 13:53:09 +0200

    Allow Heroku to deploy storage

--- storage/src/index.ts
+++ storage/src/index.ts
@@ -5,7 +5,7 @@ import { app } from './app';
 mongoose
     .connect(process.env.MONGO_DB_CONN_STRING!)
     .then(() => {
-        app.listen(5000, () => {
+        app.listen(process.env.PORT || 5000, () => {
             console.log("Storage has started!");
         });
     });
