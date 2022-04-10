e1b711144cf003aad7d1861af180e4b65f7c6175
Alvin Sartor <alvinsartor@gmail.com>
2022-04-10 20:59:53 +0200

    [DAEM-169] Deploy admin to heroku

--- admin/src/index.ts
+++ admin/src/index.ts
@@ -1,9 +1,16 @@
+import express from "express";
 import dotenv from "dotenv";
 import { scheduler } from "./cron";
 
 dotenv.config();
 
-if (!process.env.ADMIN_KEY) throw new Error('ADMIN_KEY was not set');
-if (!process.env.STORAGE_ENDPOINT) throw new Error('STORAGE_ENDPOINT was not set');
+if (!process.env.ADMIN_KEY) throw new Error("ADMIN_KEY was not set");
+if (!process.env.STORAGE_ENDPOINT) throw new Error("STORAGE_ENDPOINT was not set");
 
-scheduler();
+const app = express();
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Admin has started! Listening on port ${port}`);
+    scheduler();
+});
--- storage/src/index.ts
+++ storage/src/index.ts
@@ -1,11 +1,9 @@
-import mongoose from 'mongoose';
-import { app } from './app';
+import mongoose from "mongoose";
+import { app } from "./app";
 
-
-mongoose
-    .connect(process.env.MONGO_DB_CONN_STRING!)
-    .then(() => {
-        app.listen(process.env.PORT || 5000, () => {
-            console.log("Storage has started!");
-        });
-    });
+mongoose.connect(process.env.MONGO_DB_CONN_STRING!).then(() => {
+  const port = process.env.PORT || 5000;
+  app.listen(port, () => {
+    console.log(`Storage has started! Listening on port ${port}`);
+  });
+});
