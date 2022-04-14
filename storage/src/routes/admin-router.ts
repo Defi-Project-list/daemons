acc2898054b575fc41735136e1367d50cf7eedbc
Alvin Sartor <alvinsartor@gmail.com>
2022-04-14 15:37:58 +0200

    [DAEM-117] Add treasury stats

--- storage/src/routes/admin-router.ts
+++ storage/src/routes/admin-router.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 import { authenticateAdmin } from "../middlewares/authentication";
+import { ITreasuryStats, TreasuryStats } from "../models/stats/treasury-stats";
 import { updateScriptStats } from "../stats/script-stats-updater";
 import { updateTransactionStats } from "../stats/transactions-stats-updater";
 import { updateUserStats } from "../stats/user-stats-updater";
@@ -22,5 +23,21 @@ adminRouter.post("/update-stats", authenticateAdmin, async (req: Request, res: R
         return res.sendStatus(200);
     } catch (error) {
         return res.status(500).send(error);
+    }
+});
+
+adminRouter.post("/treasury-stats", authenticateAdmin, async (req: Request, res: Response) => {
+    // delete all stats from today to prevent duplicates
+    const today = new Date().toISOString().slice(0, 10);
+    await TreasuryStats.deleteMany({ date: today });
+
+    try {
+        const stats: ITreasuryStats[] = req.body;
+        stats.forEach(stat => stat.date = today);
+
+        await TreasuryStats.insertMany(stats);
+        return res.send();
+    } catch (error) {
+        return res.status(400).send(error);
     }
 });
