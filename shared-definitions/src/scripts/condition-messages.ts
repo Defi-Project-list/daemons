a943d6a09b95cc0ead7a4037018b314c66d71a7c
Alvin Sartor <alvinsartor@gmail.com>
2022-04-15 16:53:45 +0200

    Add contract address to health factor condition

--- shared-definitions/src/scripts/condition-messages.ts
+++ shared-definitions/src/scripts/condition-messages.ts
@@ -72,12 +72,14 @@ export const Follow = [
 
 export interface IHealthFactorCondition {
     enabled: boolean;
+    kontract: string;
     comparison: ComparisonType;
     amount: BigNumber;
 }
 
 export const HealthFactor = [
     { name: "enabled", type: "bool" },                // indicates whether the condition should be checked
+    { name: "kontract", type: "address" },           // the MM contract to interact with
     { name: "comparison", type: "bytes1" },           // the comparison symbol [">", "<"]
     { name: "amount", type: "uint256" },              // the threshold that will trigger the condition
 ];
