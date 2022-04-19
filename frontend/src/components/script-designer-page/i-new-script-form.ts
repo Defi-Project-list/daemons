cc12dc0427f5bce1cc36f3b661adf87c0af79518
Alvin Sartor <alvinsartor@gmail.com>
2022-04-19 20:13:44 +0200

    Remove old code

--- frontend/src/components/script-designer-page/i-new-script-form.ts
+++ frontend/src/components/script-designer-page/i-new-script-form.ts
@@ -3,12 +3,3 @@ import { IScriptActionForm } from '../../data/chains-data/action-form-interfaces
 export interface INewScriptForm {
     valid: boolean;
 }
-
-// export interface INewScriptBundle {
-//     frequencyCondition: IFrequencyConditionForm;
-//     balanceCondition: IBalanceConditionForm;
-//     priceCondition: IPriceConditionForm;
-//     repetitionsCondition: IRepetitionsConditionForm;
-//     followCondition: IFollowConditionForm;
-//     actionForm: IScriptActionForm;
-// }
