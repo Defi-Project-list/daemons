2ef93d4aa918c63c57118ad400547552287e9aeb
Alvin Sartor <alvinsartor@gmail.com>
2022-04-28 21:33:28 +0200

    [DAEM-207] Add DEX contract address in swap message

--- shared-definitions/src/scripts/swap-action-messages.ts
+++ shared-definitions/src/scripts/swap-action-messages.ts
@@ -17,6 +17,7 @@ export interface ISwapAction {
     typeAmt: AmountType;
     amount: BigNumber;
     user: string;
+    kontract: string;
     executor: string;
     chainId: BigNumber;
     balance: IBalanceCondition;
@@ -27,19 +28,20 @@ export interface ISwapAction {
 }
 
 const Swap = [
-    { name: "scriptId", type: "bytes32" },                  // the token owned by the user
-    { name: "tokenFrom", type: "address" },             // the token owned by the user
-    { name: "tokenTo", type: "address" },                  // the token that should be swapped
-    { name: "typeAmt", type: "bytes1" },                   // indicated the amount type [Absolute, Percentage]
-    { name: "amount", type: "uint256" },                   // the amount to swap
-    { name: "user", type: "address" },                        // the user that is signing the transaction
-    { name: "executor", type: "address" },                 // the executor contract this message will be sent to
-    { name: "chainId", type: "uint256" },                    // the chain in which the message was signed
-    { name: "balance", type: "Balance" },                   // condition: balance
-    { name: "frequency", type: "Frequency" },           // condition: frequency
-    { name: "price", type: "Price" },                            // condition: balance
-    { name: "repetitions", type: "Repetitions" },        // condition: max repetitions
-    { name: "follow", type: "Follow" },                       // condition: follow script
+    { name: "scriptId", type: "bytes32" },            // the token owned by the user
+    { name: "tokenFrom", type: "address" },           // the token owned by the user
+    { name: "tokenTo", type: "address" },             // the token that should be swapped
+    { name: "typeAmt", type: "bytes1" },              // indicated the amount type [Absolute, Percentage]
+    { name: "amount", type: "uint256" },              // the amount to swap
+    { name: "user", type: "address" },                // the user that is signing the transaction
+    { name: "kontract", type: "address" },            // the pool that will execute the swap
+    { name: "executor", type: "address" },            // the executor contract this message will be sent to
+    { name: "chainId", type: "uint256" },             // the chain in which the message was signed
+    { name: "balance", type: "Balance" },             // condition: balance
+    { name: "frequency", type: "Frequency" },         // condition: frequency
+    { name: "price", type: "Price" },                 // condition: balance
+    { name: "repetitions", type: "Repetitions" },     // condition: max repetitions
+    { name: "follow", type: "Follow" },               // condition: follow script
 ];
 
 export const swapDomain = {
