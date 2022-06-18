5510241da65fbaf352327335a78ebe97fe68441b
Alvin Sartor <alvinsartor@gmail.com>
2022-06-18 14:30:31 +0200

    [DAEM-238] Update price condition use a DEX pool instead of oracles

--- npm-packages/shared-definitions/src/conditions/price.ts
+++ npm-packages/shared-definitions/src/conditions/price.ts
@@ -3,14 +3,18 @@ import { ComparisonType } from "./shared";
 
 export interface IPriceCondition {
     enabled: boolean;
-    token: string;
+    tokenA: string;
+    tokenB: string;
     comparison: ComparisonType;
     value: BigNumber;
+    router: string;
 }
 
 export const Price = [
-    { name: "enabled", type: "bool" },                    // indicates whether the condition should be checked
-    { name: "token", type: "address" },                   // the token that will be checked
-    { name: "comparison", type: "bytes1" },           // the comparison symbol [">", "<"]
-    { name: "value", type: "uint256" },                    // the threshold that will trigger the condition
+    { name: "enabled", type: "bool" },          // indicates whether the condition should be checked
+    { name: "tokenA", type: "address" },        // the token that will be checked
+    { name: "tokenB", type: "address" },        // the token representing the price threshold
+    { name: "comparison", type: "bytes1" },     // the comparison symbol [">", "<"]
+    { name: "value", type: "uint256" },         // the threshold that will trigger the condition
+    { name: "router", type: "address" },        // the DEX that will be used to check the price
 ];
