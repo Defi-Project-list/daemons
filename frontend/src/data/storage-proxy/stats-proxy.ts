e7174312d184593d209c08075f68ee4a1bb6d11a
Alvin Sartor <alvinsartor@gmail.com>
2022-06-24 20:51:23 +0200

    Add Cacher class

--- frontend/src/data/storage-proxy/stats-proxy.ts
+++ frontend/src/data/storage-proxy/stats-proxy.ts
@@ -1,10 +1,6 @@
 import { storageAddress } from ".";
 import fetch from "cross-fetch";
-
-interface ICachedStats {
-    timestamp: number;
-    data: any[];
-}
+import { Cacher } from "../cacher";
 
 export interface IUserStat {
     date: string;
@@ -24,64 +20,26 @@ export interface ITransactionsStats {
 }
 
 export class StatsProxy {
-    private static storeData(key: string, data: any[]): void {
-        const cachedData: ICachedStats = { timestamp: Date.now(), data };
-        window.localStorage.setItem(key, JSON.stringify(cachedData));
-    }
+    private static async fetchStats(endpoint: string): Promise<any[]> {
+        const url = `${storageAddress}${endpoint}`;
+        const requestOptions = { method: "GET", credentials: "include" };
 
-    private static fetchCachedData(key: string): any[]|undefined {
-        const cache = window.localStorage.getItem(key);
-        if (!cache) return;
-
-        const maxTimestamp = 1000 * 60 * 60 * 6; // 6 hours
-        const cachedData = JSON.parse(cache);
-        if (Date.now() - cachedData.timestamp > maxTimestamp) return;
-
-        return cachedData.data;
+        const response = await fetch(url, requestOptions as any);
+        return await response.json();
     }
 
     public static async getUserStats(chainId: string): Promise<IUserStat[]> {
         const endpoint = `/stats/users/${chainId}`;
-        const cachedData = this.fetchCachedData(endpoint);
-        if (cachedData) return cachedData;
-
-        const url = `${storageAddress}${endpoint}`;
-        const requestOptions = { method: "GET", credentials: "include" };
-
-        const response = await fetch(url, requestOptions as any);
-        const stats = await response.json();
-        this.storeData(endpoint, stats)
-
-        return stats;
+        return await Cacher.fetchData(endpoint, () => this.fetchStats(endpoint));
     }
 
     public static async getScriptStats(chainId: string): Promise<IScriptStats[]> {
         const endpoint = `/stats/scripts/${chainId}`;
-        const cachedData = this.fetchCachedData(endpoint);
-        if (cachedData) return cachedData;
-
-        const url = `${storageAddress}${endpoint}`;
-        const requestOptions = { method: "GET", credentials: "include" };
-
-        const response = await fetch(url, requestOptions as any);
-        const stats = await response.json();
-        this.storeData(endpoint, stats)
-
-        return stats;
+        return await Cacher.fetchData(endpoint, () => this.fetchStats(endpoint));
     }
 
     public static async getTransactionsStats(chainId: string): Promise<ITransactionsStats[]> {
         const endpoint = `/stats/transactions/${chainId}`;
-        const cachedData = this.fetchCachedData(endpoint);
-        if (cachedData) return cachedData;
-
-        const url = `${storageAddress}${endpoint}`;
-        const requestOptions = { method: "GET", credentials: "include" };
-
-        const response = await fetch(url, requestOptions as any);
-        const stats = await response.json();
-        this.storeData(endpoint, stats)
-
-        return stats;
+        return await Cacher.fetchData(endpoint, () => this.fetchStats(endpoint));
     }
 }
