96a80a28c484e120489e030adf51d078c9428a10
Daemons <info@daemons.fi>
2022-07-16 16:59:46 +0200

    Update transaction definition in NPM package

--- npm-packages/shared-definitions/src/transactions/transaction.ts
+++ npm-packages/shared-definitions/src/transactions/transaction.ts
@@ -1,20 +1,13 @@
-import { BigNumber } from 'ethers';
-
-export enum TransactionOutcome {
-    Waiting = 'W',
-    Confirmed = 'C',
-    Failed = 'F',
-    NotFound = 'NF',
-}
-
 export interface ITransaction {
     hash: string;
-    chainId: BigNumber;
+    chainId: string;
     scriptId: string;
     scriptType: string;
     description: string;
     executingUser: string;
     beneficiaryUser: string;
-    date: Date;
-    outcome: TransactionOutcome;
+    timestamp: number;
+    costEth: number;
+    costDAEM: number;
+    profitDAEM: number;
 }
