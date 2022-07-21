48d2f67992bc08e02601dbda1dd54b1e9939b3a7
Daemons <info@daemons.fi>
2022-07-21 10:11:33 +0200

    Bump storage

--- storage/src/index.ts
+++ storage/src/index.ts
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
-import { app } from "./app";
+import { ExpressApp } from "./app";
 import { scheduler } from "./cron";
 
 mongoose.connect(process.env.MONGO_DB_CONN_STRING!).then(() => {
   const port = process.env.PORT || 5000;
+  const app = ExpressApp.instantiate();
   app.listen(port, () => {
     console.log(`Storage has started! Listening on port ${port}`);
     scheduler();
