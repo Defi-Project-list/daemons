73ab994340e8189b7c3265f12c86066fe7dc0a0b
Daemons <info@daemons.fi>
2022-07-21 11:17:22 +0200

    Try to get the storage deployment action working

--- storage/src/app.ts
+++ storage/src/app.ts
@@ -9,31 +9,25 @@ import { transactionsRouter } from "./routes/transactions-router";
 import { statisticsRouter } from "./routes/statistics-router";
 import { notificationsRouter } from "./routes/notifications-router";
 
-export class ExpressApp {
-    public static instantiate() {
-        dotenv.config();
+dotenv.config();
 
-        if (!process.env.JWT_SECRET) throw new Error("JWT_SECRET was not set");
-        if (!process.env.ADMIN_KEY) throw new Error("ADMIN_KEY was not set");
-        if (!process.env.MONGO_DB_CONN_STRING) throw new Error("MONGO_DB_CONN_STRING was not set");
+if (!process.env.JWT_SECRET) throw new Error("JWT_SECRET was not set");
+if (!process.env.ADMIN_KEY) throw new Error("ADMIN_KEY was not set");
+if (!process.env.MONGO_DB_CONN_STRING) throw new Error("MONGO_DB_CONN_STRING was not set");
 
-        // RPC addresses
-        if (!process.env.KOVAN_RPC) throw new Error("KOVAN_RPC was not set");
-        if (!process.env.FANTOM_TESTNET_RPC) throw new Error("FANTOM_TESTNET_RPC was not set");
+// RPC addresses
+if (!process.env.KOVAN_RPC) throw new Error("KOVAN_RPC was not set");
+if (!process.env.FANTOM_TESTNET_RPC) throw new Error("FANTOM_TESTNET_RPC was not set");
 
-        const app = express();
+export const app = express();
 
-        app.use(corsWhitelisting);
-        app.use(express.json());
-        app.use(cookieParser());
+app.use(corsWhitelisting);
+app.use(express.json());
+app.use(cookieParser());
 
-        app.use("/api/auth", authenticationRouter);
-        app.use("/api/scripts", scriptsRouter);
-        app.use("/api/transactions", transactionsRouter);
-        app.use("/api/admin", adminRouter);
-        app.use("/api/stats", statisticsRouter);
-        app.use("/api/notifications", notificationsRouter);
-
-        return app;
-    }
-}
+app.use("/api/auth", authenticationRouter);
+app.use("/api/scripts", scriptsRouter);
+app.use("/api/transactions", transactionsRouter);
+app.use("/api/admin", adminRouter);
+app.use("/api/stats", statisticsRouter);
+app.use("/api/notifications", notificationsRouter);
--- storage/src/index.ts
+++ storage/src/index.ts
@@ -1,10 +1,9 @@
 import mongoose from "mongoose";
-import { ExpressApp } from "./app";
+import { app } from "./app";
 import { scheduler } from "./cron";
 
 mongoose.connect(process.env.MONGO_DB_CONN_STRING!).then(() => {
   const port = process.env.PORT || 5000;
-  const app = ExpressApp.instantiate();
   app.listen(port, () => {
     console.log(`Storage has started! Listening on port ${port}`);
     scheduler();
