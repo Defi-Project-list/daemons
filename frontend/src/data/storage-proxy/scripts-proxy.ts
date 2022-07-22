5b64dd16c23422ed16e75f80c7a6eb91e868944d
Daemons <info@daemons.fi>
2022-07-22 15:13:29 +0200

    Use parseScript from script-definitions instead of duplicating code in frontend

--- frontend/src/data/storage-proxy/scripts-proxy.ts
+++ frontend/src/data/storage-proxy/scripts-proxy.ts
@@ -1,5 +1,4 @@
-import { BaseScript, BeefyScript, PassScript, SwapScript, TransferScript, ZapInScript, ZapOutScript } from "@daemons-fi/scripts-definitions";
-import { MmBaseScript, MmAdvancedScript } from "@daemons-fi/scripts-definitions";
+import { BaseScript, parseScript } from "@daemons-fi/scripts-definitions";
 import { storageAddress } from ".";
 
 
@@ -29,7 +28,7 @@ export class ScriptProxy {
         const json: any[] = await response.json();
         const scripts: BaseScript[] = [];
         for (const script of json) {
-            scripts.push(await this.parseScript(script));
+            scripts.push(await parseScript(script));
         }
 
         return scripts;
@@ -51,33 +50,10 @@ export class ScriptProxy {
         const json: any[] = await response.json();
         const scripts: BaseScript[] = [];
         for (const script of json) {
-            scripts.push(await this.parseScript(script));
+            scripts.push(await parseScript(script));
         }
 
         return scripts;
-    }
-
-    private static async parseScript(script: any): Promise<BaseScript> {
-        switch (script.__type) {
-            case "SwapScript":
-                return await SwapScript.fromStorageJson(script);
-            case "TransferScript":
-                return await TransferScript.fromStorageJson(script);
-            case "MmBaseScript":
-                return await MmBaseScript.fromStorageJson(script);
-            case "MmAdvancedScript":
-                return await MmAdvancedScript.fromStorageJson(script);
-            case "ZapInScript":
-                return await ZapInScript.fromStorageJson(script);
-            case "ZapOutScript":
-                return await ZapOutScript.fromStorageJson(script);
-            case "BeefyScript":
-                return await BeefyScript.fromStorageJson(script);
-            case "PassScript":
-                return await PassScript.fromStorageJson(script);
-            default:
-                throw new Error("Unsupported script type: " + script.__type);
-        }
     }
 
     public static async updateScriptDescription(
