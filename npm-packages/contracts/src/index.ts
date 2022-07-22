46290aef5b212ce6d9f551129460fffffe6ab209
Daemons <info@daemons.fi>
2022-07-22 15:31:41 +0200

    Merge "ABIs" and "Addresses" packages into "Contracts"

--- npm-packages/contracts/src/index.ts
+++ npm-packages/contracts/src/index.ts
@@ -1,11 +1,14 @@
-// core
+// Addresses
+export * from "./addresses";
+
+// core ABIs
 export * from "./ABIs/core/DAEM";
 export * from "./ABIs/core/gas-price-feed";
 export * from "./ABIs/core/gas-tank";
 export * from "./ABIs/core/treasury";
 export * from "./ABIs/core/vesting";
 
-// executors
+// executors ABIs
 export * from "./ABIs/script-executors/mm-base-script";
 export * from "./ABIs/script-executors/mm-advanced-script";
 export * from "./ABIs/script-executors/swap-script";
@@ -15,10 +18,13 @@ export * from "./ABIs/script-executors/zap-out-script";
 export * from "./ABIs/script-executors/beefy-script";
 export * from "./ABIs/script-executors/pass-script";
 
-// utils
+// utils ABIs
 export * from "./ABIs/utils/erc20";
 export * from "./ABIs/utils/credit-delegation-token";
 export * from "./ABIs/utils/money-market";
 export * from "./ABIs/utils/uniswap-V2-router";
 export * from "./ABIs/utils/uniswap-V2-factory";
 export * from "./ABIs/utils/uniswap-V2-pair";
+
+// utils
+export * from "./utils/big-number-to-float";
