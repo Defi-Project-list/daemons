7c50e292b3bb7424964964d747023a3d48a96eb4
Daemons <info@daemons.fi>
2022-07-23 14:25:51 +0200

    Fix issue in wallet indicator styling

--- frontend/src/pages/error-pages/disconnected-page.tsx
+++ frontend/src/pages/error-pages/disconnected-page.tsx
@@ -1,13 +1,14 @@
-import React, { Component, ReactNode } from 'react';
-import './styles.css';
+import React, { Component, ReactNode } from "react";
+import "./styles.css";
 
 export class DisconnectedPage extends Component {
-
     public render(): ReactNode {
         return (
-            <div className="disconnected">
-                Connect via Metamask and prove you own that address to access Daemons.
-            </div>
+            <>
+                <div className="disconnected">
+                    Connect via Metamask and prove you own that address to access Daemons.
+                </div>
+            </>
         );
     }
 }
